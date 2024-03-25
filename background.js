69808fedd0ed9a7bedec5bc104ad0a871a673a91
StickySputum <Bro.5n3k1t@yandex.ru>
2024-03-25 04:36:23 +0300

    finally working

--- background.js
+++ background.js
@@ -1,5 +1,44 @@
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, message);
+// background.js
+
+const nektoMeSite = 'nekto.me';
+let var_interval;
+
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.setBadgeText({
+    text: 'OFF'
   });
+});
+
+chrome.action.onClicked.addListener(async (tab) => {
+  if (tab.url.includes(nektoMeSite)) {
+    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+
+    await chrome.action.setBadgeText({
+      tabId: tab.id,
+      text: nextState
+    });
+
+    clearInterval(var_interval);
+
+
+    if (nextState === 'ON') {
+      var_interval = setInterval(() => {
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          files: ['content.js']
+        });
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          function: 'findAndClickButton'
+        });
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          function: 'checkAndRedirect'
+        });
+      }, 2000);
+      
+    }
+  }
 });
--- content.js
+++ content.js
@@ -1,11 +1,7 @@
-let isOn = false;
-let button_search_timeout;
-let redirect_timeout;
+// content.js
 
-// Функция для поиска кнопки и нажатия
 function findAndClickButton() {
-    console.log(isOn);
-    if (document.location.href === 'https://nekto.me/audiochat#/' && isOn) {
+    if (document.location.href === 'https://nekto.me/audiochat#/') {
         var button = document.getElementById('searchCompanyBtn');
         if (button) {
             button.click();
@@ -13,27 +9,16 @@ function findAndClickButton() {
     }
 }
 
-// Непрерывная проверка наличия кнопки и нажатие только при включенной кнопке
-button_search_timeout = setInterval(findAndClickButton, 2000);
-
-// Вызов функции сразу после загрузки страницы
-findAndClickButton();
-
-// Добавленный код для переключения на страницу, если текущий URL содержит '/peer'
 function checkAndRedirect() {
-    console.log(isOn);
-    if (document.location.href.includes('/peer') && isOn) {
-        // Создание элемента <a> для редиректа
-        var redirectLink = document.createElement('a');
-        redirectLink.href = 'https://nekto.me/audiochat#/';
-    
-        // Симуляция клика по элементу для перенаправления
-        redirectLink.click();
+    if (document.location.href.includes('/peer')) {
+        // Изменено на поиск элемента по классу
+        var redirectLink = document.querySelector('.btn.btn-lg.go-idle-button');
+        if (redirectLink) {
+            // Если кнопка найдена, симулируем клик на неё
+            redirectLink.click();
+        }
     }
 }
 
-// Непрерывная проверка изменения URL только при включенной кнопке
-redirect_timeout = setInterval(checkAndRedirect, 3000);
-
-// Вызов функции сразу после загрузки страницы
+findAndClickButton();
 checkAndRedirect();
