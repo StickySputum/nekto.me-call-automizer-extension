4e2866142c39344e8afb387db41100394a611cd4
StickySputum <Bro.5n3k1t@yandex.ru>
2024-03-25 05:20:26 +0300

    Added storage and extensions state

--- background.js
+++ background.js
@@ -3,9 +3,41 @@
 const nektoMeSite = 'nekto.me';
 let var_interval;
 
+const setBadgeTextOnTab = (tabId, text) => {
+  chrome.action.setBadgeText({ tabId, text });
+};
+
+// Обработчик для события обновления вкладки
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.url.includes(nektoMeSite)) {
+    getStateFromStorage(tabId).then((state) => {
+      setBadgeTextOnTab(tabId, state);
+    });
+  }
+});
+
+// Функция для получения состояния из хранилища
+const getStateFromStorage = async (tabId) => {
+  return new Promise((resolve) => {
+    chrome.storage.local.get(['badgeState'], (result) => {
+      const badgeState = result.badgeState || {};
+      resolve(badgeState[tabId] || 'OFF');
+    });
+  });
+};
+
+// Функция для сохранения состояния в хранилище
+const saveStateToStorage = (tabId, state) => {
+  chrome.storage.local.get(['badgeState'], (result) => {
+    const badgeState = result.badgeState || {};
+    badgeState[tabId] = state;
+    chrome.storage.local.set({ badgeState });
+  });
+};
+
 chrome.action.onClicked.addListener(async (tab) => {
   if (tab.url.includes(nektoMeSite)) {
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+    const prevState = await getStateFromStorage(tab.id);
     const nextState = prevState === 'ON' ? 'OFF' : 'ON';
 
     await chrome.action.setBadgeText({
@@ -14,7 +46,6 @@ chrome.action.onClicked.addListener(async (tab) => {
     });
 
     clearInterval(var_interval);
-
 
     if (nextState === 'ON') {
       var_interval = setInterval(() => {
@@ -31,7 +62,8 @@ chrome.action.onClicked.addListener(async (tab) => {
           function: 'checkAndRedirect'
         });
       }, 2000);
-      
     }
+
+    saveStateToStorage(tab.id, nextState);
   }
 });
